241be4bc09743f35c53a33cbea77daf9e984fbe1
Linga Raghavendra <lingaraghawendra@gmail.com>
2026-08-17 09:41:36 +0530

    Gray_Code

---
+++ Introductory Problems/Gray_Code.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define vi vector<int>
+
+int testcase = 0;
+void solve(){
+    int n;
+    cin >> n;
+    
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t=1;
+    if(testcase) cin>>t;
+    while(t--) solve();
+    return 0;
+}
