3bdc86fe9f1513d659899683d551658c942e92da
Linga Raghavendra <lingaraghawendra@gmail.com>
2026-08-17 09:41:36 +0530

    Trailing_Zeros

---
+++ Introductory Problems/Trailing_Zeros.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define vi vector<int>
+
+int testcase = 0;
+void solve(){
+    int n;
+    cin >> n;
+    int ans = 0;
+    int mul = 5;
+    while(n/mul > 0){
+        ans += n/mul;
+        mul *= 5;
+    }
+    cout << ans << endl;
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t=1;
+    if(testcase) cin>>t;
+    while(t--) solve();
+    return 0;
+}
