3b77152a0117c80d4e3820481fdcbbdd562d1f1d
Linga Seetha Rama Raghavendra <lingaraghawendra@gmail.com>
2026-08-19 10:42:33 +0530

    docs: present CSES solutions cleanly (#2)
    * docs: add CSES solutions overview
    
    * fix: implement Nested Ranges Count

--- Sorting and Searching/Nested_Ranges_Count.cpp
+++ Sorting and Searching/Nested_Ranges_Count.cpp
@@ -1,35 +1,101 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
-
-
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
 using namespace std;
-using namespace __gnu_pbds;
 
+class FenwickTree {
+public:
+    explicit FenwickTree(int size) : tree(size + 1, 0) {}
 
-typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> oset;
+    void add(int index, int value) {
+        for (; index < static_cast<int>(tree.size()); index += index & -index) {
+            tree[index] += value;
+        }
+    }
 
-#define int long long
-#define endl "\n"
-#define all(x) (x).begin(), (x).end()
-#define vi vector<int>
+    int prefix_sum(int index) const {
+        int result = 0;
+        for (; index > 0; index -= index & -index) {
+            result += tree[index];
+        }
+        return result;
+    }
 
-int testcase = 0;
-void solve(){
-    // write code here
-    oset o;
-    o.find_by_order();
-    o.order_of_key();
-}
+private:
+    vector<int> tree;
+};
 
-int32_t main(){
+struct Range {
+    int left;
+    int right;
+    int original_index;
+};
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t=1;
-    if(testcase) cin>>t;
-    while(t--) solve();
+    int n;
+    cin >> n;
+
+    vector<Range> ranges(n);
+    vector<int> right_endpoints;
+    right_endpoints.reserve(n);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> ranges[i].left >> ranges[i].right;
+        ranges[i].original_index = i;
+        right_endpoints.push_back(ranges[i].right);
+    }
+
+    sort(ranges.begin(), ranges.end(), [](const Range& first, const Range& second) {
+        if (first.left != second.left) {
+            return first.left < second.left;
+        }
+        return first.right > second.right;
+    });
+
+    sort(right_endpoints.begin(), right_endpoints.end());
+    right_endpoints.erase(
+        unique(right_endpoints.begin(), right_endpoints.end()),
+        right_endpoints.end()
+    );
+
+    auto compressed_index = [&](int right) {
+        return static_cast<int>(
+            lower_bound(right_endpoints.begin(), right_endpoints.end(), right)
+            - right_endpoints.begin()
+        ) + 1;
+    };
+
+    vector<int> contains(n, 0);
+    vector<int> contained_by(n, 0);
+
+    FenwickTree fenwick(static_cast<int>(right_endpoints.size()));
+    for (int i = n - 1; i >= 0; --i) {
+        int index = compressed_index(ranges[i].right);
+        contains[ranges[i].original_index] = fenwick.prefix_sum(index);
+        fenwick.add(index, 1);
+    }
+
+    fenwick = FenwickTree(static_cast<int>(right_endpoints.size()));
+    for (int i = 0; i < n; ++i) {
+        int index = compressed_index(ranges[i].right);
+        contained_by[ranges[i].original_index] =
+            i - fenwick.prefix_sum(index - 1);
+        fenwick.add(index, 1);
+    }
+
+    for (int count : contains) {
+        cout << count << ' ';
+    }
+    cout << '\n';
+
+    for (int count : contained_by) {
+        cout << count << ' ';
+    }
+    cout << '\n';
+
     return 0;
 }
